f64cfda21fb5bbab08b4f854ada85fe7760b41b6
FatFrederik <dorhasenbroek@gmail.com>
2021-11-01 15:36:29 +0100

    main

--- src/main/java/nl/vu/labs/phoenix/ap/Main.java
+++ src/main/java/nl/vu/labs/phoenix/ap/Main.java
@@ -1,17 +1,33 @@
 package nl.vu.labs.phoenix.ap;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
-public class Main {
-	
+public class Main{
+
 	private void start() {
 		InterpreterInterface<Set<BigInteger>> interpreter = new Interpreter<Set<BigInteger>>();
-		
-		//1. Create a scanner on System.in
-		//2. call interpreter.eval() on each line
+		Scanner in = new Scanner(System.in);
+		while (in.hasNextLine()) {
+			Set set = interpreter.eval(in.nextLine());
+			if (set != null) {
+				printSet(set);
+
+			}
+			//1. Create a scanner on System.in
+			//2. call interpreter.eval() on each line
+
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		new Main().start();
 	}
+
+	private void printSet(Set s){
+		T id = s.get();
+
+
+	}
+
 }
