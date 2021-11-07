fbec7e063d74c2b8b262b1ccc62995055519986a
FatFrederik <dorhasenbroek@gmail.com>
2021-11-07 16:15:31 +0100

    print function improved

--- src/main/java/nl/vu/labs/phoenix/ap/Interpreter.java
+++ src/main/java/nl/vu/labs/phoenix/ap/Interpreter.java
@@ -8,253 +8,248 @@ import java.util.regex.Pattern;
 
 public class Interpreter<T extends SetInterface<BigInteger>> implements InterpreterInterface<T> {
 
-	HashMap<Identifier, T> hashMap;
+    HashMap<Identifier, T> hashMap;
 
-	public Interpreter(){
-		hashMap = new HashMap<Identifier, T>();
-	}
+    public Interpreter() {
+        hashMap = new HashMap<Identifier, T>();
+    }
 
-	@Override
-	public T getMemory(String v) {
-		Scanner in = new Scanner(v);
-		in.useDelimiter("");
-		Identifier identifier = new Identifier();
-		try{
-			identifier = identifier(in);
-		} catch(Exception e){
-			System.out.println(e.getMessage());
-		}
-		return hashMap.get(identifier);
-	}
+    @Override
+    public T getMemory(String v) {
+        Scanner in = new Scanner(v);
+        in.useDelimiter("");
+        Identifier identifier = new Identifier();
+        try {
+            identifier = identifier(in);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return hashMap.get(identifier);
+    }
 
-	@Override
-	public T eval(String s) {
-		Scanner in = new Scanner(s);
-		in.useDelimiter("");
-		try{
-			 return program(in);
-		} catch(APException e){
-			System.out.println(e.getMessage());
-			return null;
-		}
-	}
+    @Override
+    public T eval(String s) {
+        Scanner in = new Scanner(s);
+        in.useDelimiter("");
+        try {
+            return program(in);
+        } catch (APException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 
-	private T program(Scanner in) throws APException{
-		return statement(in);
-	}
+    private T program(Scanner in) throws APException {
+        return statement(in);
+    }
 
-	private T statement(Scanner in) throws APException{
-		skipSpaces(in);
-		if(nextCharIsLetter(in)){
-			assignment(in);
-		}
-		else if(nextCharIs(in, '?')){
-			nextChar(in);
-			return print_statement(in);
-		}
-		else if(nextCharIs(in, '/')){
-			nextChar(in);
-			comment(in);
-		}
-		else{
-			throw new APException("Command needs to start with a statement");
-		}
-		return null;
-	}
+    private T statement(Scanner in) throws APException {
+        skipSpaces(in);
+        if (nextCharIsLetter(in)) {
+            assignment(in);
+        } else if (nextCharIs(in, '?')) {
+            nextChar(in);
+            return print_statement(in);
+        } else if (nextCharIs(in, '/')) {
+            nextChar(in);
+            comment(in);
+        } else {
+            throw new APException("Command needs to start with a statement");
+        }
+        return null;
+    }
 
 
-	private void assignment(Scanner in) throws APException{
-		Identifier id = identifier(in);
+    private void assignment(Scanner in) throws APException {
+        Identifier id = identifier(in);
 
-		skipSpaces(in);
-		if(!(nextCharIs(in, '='))){
-			throw new APException("an assignment needs to consist of an identifier followed by = followed by an expression");
-		}
-		nextChar(in);
+        skipSpaces(in);
+        if (!(nextCharIs(in, '='))) {
+            throw new APException("an assignment needs to consist of an identifier followed by = followed by an expression");
+        }
+        nextChar(in);
 
-		T expr = expression(in);
+        T expr = expression(in);
 
-		eoln(in);
-
-
-		hashMap.put(id, expr);
-	}
-
-	private T print_statement(Scanner in) throws APException {
-		T print = expression(in);
-		eoln(in);
-		return print;
-	}
-	private void comment(Scanner in){
-		//goes back to program
-	}
-
-	private Identifier identifier(Scanner in) {
-		Identifier id = new Identifier();
-		id.add(nextChar(in));
-		while(nextCharIsLetter(in) || nextCharIsDigit(in)){
-			id.add(nextChar(in));
-		}
-		return id;
-	}
-
-	private T expression(Scanner in) throws APException {
-		char operator;
-		skipSpaces(in);
-		T t = term(in);
-		skipSpaces(in);
-		while (nextCharIs(in, '+') || nextCharIs(in, '-') || nextCharIs(in, '|')) {
-			operator = nextChar(in);
-			skipSpaces(in);
-			t = calculate(t, term(in), operator);
-			skipSpaces(in);
-		}
-		return t;
-	}
+        eoln(in);
 
 
+        hashMap.put(id, expr);
+    }
 
-	private T term(Scanner in) throws APException{
-		char operator;
-		T f = factor(in);
-		skipSpaces(in);
-		while (nextCharIs(in, '*')) {
-			operator = nextChar(in);
-			skipSpaces(in);
-			f = calculate(f, factor(in), operator);
-			skipSpaces(in);
-		}
-		return f;
-	}
+    private T print_statement(Scanner in) throws APException {
+        T print = expression(in);
+        eoln(in);
+        return print;
+    }
 
-	private T factor(Scanner in) throws APException{
-		skipSpaces(in);
-		if(nextCharIsLetter(in)){
-			Identifier id = identifier(in);
-			if (hashMap.containsKey(id)) {
-				return hashMap.get(id);
-			}
-		}
-		else if(nextCharIs(in, '(')){
-			nextChar(in);
-			skipSpaces(in);
-			return complex_factor(in);
-		}
-		else if(nextCharIs(in, '{')){
-			nextChar(in);
-			return set(in);
-		}
-		throw new APException("Factor should be a set, identifier, or a complex factor");
-	}
+    private void comment(Scanner in) {
+        //goes back to program
+    }
 
-	private T complex_factor(Scanner in) throws APException{
-		T cf = expression(in);
-		skipSpaces(in);
-		if(!nextCharIs(in, ')')){
-			throw new APException("complex factors should end with ')'");
-		}
-		nextChar(in);
-		return cf;
-	}
+    private Identifier identifier(Scanner in) {
+        Identifier id = new Identifier();
+        id.add(nextChar(in));
+        while (nextCharIsLetter(in) || nextCharIsDigit(in)) {
+            id.add(nextChar(in));
+        }
+        return id;
+    }
 
-	private T set(Scanner in) throws APException{
-		T set = (T) new Set<BigInteger>();
+    private T expression(Scanner in) throws APException {
+        char operator;
+        skipSpaces(in);
+        T t = term(in);
+        skipSpaces(in);
+        while (nextCharIs(in, '+') || nextCharIs(in, '-') || nextCharIs(in, '|')) {
+            operator = nextChar(in);
+            skipSpaces(in);
+            t = calculate(t, term(in), operator);
+            skipSpaces(in);
+        }
+        return t;
+    }
 
-		skipSpaces(in);
-		if(nextCharIsDigit(in)){
-			row_natural_numbers(in, set);
-		}
-		if(nextCharIs(in, '}')){
-			nextChar(in);
-		} else{
-			throw new APException("A set can only contain a row of natural numbers and should be closed with '}'");
-		}
-		return set;
-	}
 
-	private T row_natural_numbers(Scanner in, T set) {
-		set.add(natural_number(in));
-		skipSpaces(in);
-		while(nextCharIs(in, ',')){
-			nextChar(in);
-			skipSpaces(in);
-			if(!nextCharIsDigit(in)){
-				return set;
-			}
-			set.add(natural_number(in));
-			skipSpaces(in);
-		}
-		return set;
-	}
+    private T term(Scanner in) throws APException {
+        char operator;
+        T f = factor(in);
+        skipSpaces(in);
+        while (nextCharIs(in, '*')) {
+            operator = nextChar(in);
+            skipSpaces(in);
+            f = calculate(f, factor(in), operator);
+            skipSpaces(in);
+        }
+        return f;
+    }
 
-	private boolean additive_Operator(char c){
-		return (c == '+' || c == '-' || c == '|');
-	}
+    private T factor(Scanner in) throws APException {
+        skipSpaces(in);
+        if (nextCharIsLetter(in)) {
+            Identifier id = identifier(in);
+            if (hashMap.containsKey(id)) {
+                return hashMap.get(id);
+            }
+        } else if (nextCharIs(in, '(')) {
+            nextChar(in);
+            skipSpaces(in);
+            return complex_factor(in);
+        } else if (nextCharIs(in, '{')) {
+            nextChar(in);
+            return set(in);
+        }
+        throw new APException("Factor should be a set, identifier, or a complex factor");
+    }
 
-	private boolean multiplicative_operator(char c) {
-		return (c == '*');
-	}
+    private T complex_factor(Scanner in) throws APException {
+        T cf = expression(in);
+        skipSpaces(in);
+        if (!nextCharIs(in, ')')) {
+            throw new APException("complex factors should end with ')'");
+        }
+        nextChar(in);
+        return cf;
+    }
 
-	private T calculate(T s1, T s2, char operator) throws APException {
-		if(operator == '+'){
-			return (T) s1.union(s2);
-		}
-		if(operator == '-'){
-			return (T) s1.difference(s2);
-		}
+    private T set(Scanner in) throws APException {
+        T set = (T) new Set<BigInteger>();
 
-		if (operator == '|'){
-			return (T) s1.symmetricDifference(s2);
-		}
+        skipSpaces(in);
+        if (nextCharIsDigit(in)) {
+            row_natural_numbers(in, set);
+        }
+        if (nextCharIs(in, '}')) {
+            nextChar(in);
+        } else {
+            throw new APException("A set can only contain a row of natural numbers and should be closed with '}'");
+        }
+        return set;
+    }
 
-		if (operator == '*'){
-			return (T) s1.intersection(s2);
-		}
-		throw new APException("Operator is not: +, -, * or |");
-	}
+    private T row_natural_numbers(Scanner in, T set) {
+        set.add(natural_number(in));
+        skipSpaces(in);
+        while (nextCharIs(in, ',')) {
+            nextChar(in);
+            skipSpaces(in);
+            if (!nextCharIsDigit(in)) {
+                return set;
+            }
+            set.add(natural_number(in));
+            skipSpaces(in);
+        }
+        return set;
+    }
 
-	private BigInteger natural_number(Scanner in){
-		if(nextCharIs(in, '0')){
-			nextChar(in);
-			return new BigInteger("0");
-		}
-		return positive_number(in);
-	}
+    private boolean additive_Operator(char c) {
+        return (c == '+' || c == '-' || c == '|');
+    }
 
-	private BigInteger positive_number(Scanner in){
-		StringBuffer number = new StringBuffer();
-		while(nextCharIsDigit(in)) {
-			number.append(nextChar(in));
-		}
-		return new BigInteger(number.toString());
-	}
+    private boolean multiplicative_operator(char c) {
+        return (c == '*');
+    }
 
-	private char nextChar(Scanner in) {
-			return in.next().charAt(0);
-	}
+    private T calculate(T s1, T s2, char operator) throws APException {
+        if (operator == '+') {
+            return (T) s1.union(s2);
+        }
+        if (operator == '-') {
+            return (T) s1.difference(s2);
+        }
 
-	private void skipSpaces(Scanner in){
-		while(nextCharIs(in, ' ')){
-			nextChar(in);
-		}
-	}
+        if (operator == '|') {
+            return (T) s1.symmetricDifference(s2);
+        }
 
-	private boolean nextCharIs(Scanner in,char c) {
-		return in.hasNext(Pattern.quote(c+""));
-	}
+        if (operator == '*') {
+            return (T) s1.intersection(s2);
+        }
+        throw new APException("Operator is not: +, -, * or |");
+    }
 
-	private boolean nextCharIsDigit(Scanner in) {
-		return in.hasNext("[0-9]");
-	}
+    private BigInteger natural_number(Scanner in) {
+        if (nextCharIs(in, '0')) {
+            nextChar(in);
+            return new BigInteger("0");
+        }
+        return positive_number(in);
+    }
 
-	private boolean nextCharIsLetter(Scanner in) {
-		return in.hasNext("[a-zA-Z]");
-	}
+    private BigInteger positive_number(Scanner in) {
+        StringBuffer number = new StringBuffer();
+        while (nextCharIsDigit(in)) {
+            number.append(nextChar(in));
+        }
+        return new BigInteger(number.toString());
+    }
 
-	private void eoln(Scanner in) throws APException {
-		skipSpaces(in);
-		if (in.hasNext()) {
-			throw new APException("Assignment should end with an expression");
-		}
-	}
+    private char nextChar(Scanner in) {
+        return in.next().charAt(0);
+    }
+
+    private void skipSpaces(Scanner in) {
+        while (nextCharIs(in, ' ')) {
+            nextChar(in);
+        }
+    }
+
+    private boolean nextCharIs(Scanner in, char c) {
+        return in.hasNext(Pattern.quote(c + ""));
+    }
+
+    private boolean nextCharIsDigit(Scanner in) {
+        return in.hasNext("[0-9]");
+    }
+
+    private boolean nextCharIsLetter(Scanner in) {
+        return in.hasNext("[a-zA-Z]");
+    }
+
+    private void eoln(Scanner in) throws APException {
+        skipSpaces(in);
+        if (in.hasNext()) {
+            throw new APException("Assignment should end with an expression");
+        }
+    }
 }
--- src/main/java/nl/vu/labs/phoenix/ap/Main.java
+++ src/main/java/nl/vu/labs/phoenix/ap/Main.java
@@ -3,35 +3,33 @@ package nl.vu.labs.phoenix.ap;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main<T extends Comparable>{
+public class Main<T extends Comparable> {
 
-	private void start() {
-		InterpreterInterface<Set<BigInteger>> interpreter = new Interpreter<Set<BigInteger>>();
-		Scanner in = new Scanner(System.in);
-		while (in.hasNextLine()) {
-			Set set = interpreter.eval(in.nextLine());
-			if (set != null) {
-				printSet(set);
+    private void start() {
+        InterpreterInterface<Set<BigInteger>> interpreter = new Interpreter<Set<BigInteger>>();
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            Set set = interpreter.eval(in.nextLine());
+            if (set != null) {
+                printSet(set);
 
-			}
-			//1. Create a scanner on System.in
-			//2. call interpreter.eval() on each line
+            }
+        }
+    }
 
-		}
-	}
+    public static void main(String[] args) {
+        new Main().start();
+    }
 
-	public static void main(String[] args) {
-		new Main().start();
-	}
-
-	private void printSet(Set<BigInteger> set) {
-		Set<BigInteger> setCopy = set.copy();
-		if (setCopy.isEmpty()) {
-			System.out.println("");
-		}
-		while (setCopy.size() != 0) {
-			System.out.print(setCopy.get());
-			setCopy.remove(setCopy.get());
-		}
-	}
+    private void printSet(Set<BigInteger> set) {
+        Set<BigInteger> setCopy = set.copy();
+        if (setCopy.isEmpty()) {
+            System.out.println("");
+        }
+        while (setCopy.size() != 0) {
+            System.out.print(setCopy.get());
+            setCopy.remove(setCopy.get());
+        }
+        System.out.print("\n");
+    }
 }
